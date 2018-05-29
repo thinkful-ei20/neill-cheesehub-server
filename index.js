353eee4333b18398e8b034eb243110d5d2e13746
neillsom <neillsom@mail.com>
2018-05-28 22:42:39 -0700

    initial commit

--- index.js
+++ index.js
@@ -22,6 +22,31 @@ app.use(
   })
 );
 
+const list = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue'
+];
+
+// GET
+app.get('/api/cheeses', (req, res ) => {
+  res.json({list});
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
